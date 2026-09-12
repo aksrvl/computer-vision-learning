5f3d7c542656a0b3226eaddd3d17811b01e0e1c3
aksrvl <96061767+aksrvl@users.noreply.github.com>
2026-09-12 21:29:58 +0300

    fde and ade calcualtions

--- 03_tracking/tracking_video.py
+++ 03_tracking/tracking_video.py
@@ -10,6 +10,9 @@ history = {}
 kalman_filters = {} #store filters for every id
 kalman_history = {} 
 predictions = {}
+fdes = {}
+ade_predictions = {}
+ades = {}
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -33,6 +36,9 @@ while cap.isOpened():
             if track_id not in predictions:
                 predictions[track_id] = []
 
+            if track_id not in ade_predictions:
+                ade_predictions[track_id] = []
+
             if track_id not in kalman_filters:
                 kf = cv.KalmanFilter(4, 2)
                 kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32) #identify which variables we want to measure
@@ -54,15 +60,16 @@ while cap.isOpened():
                 state_pre = kf.predict()
                 kf.correct(measurement)
                 state_post = kf.statePost
-
+                forecast = {"predictions": [], "errors": []}
                 for i in range(1, 11):
                     future_x = state_post[0][0] + i*state_post[2][0]
                     future_y = state_post[1][0] + i*state_post[3][0]
+                    forecast["predictions"].append((frame_count + i, future_x, future_y))
                     cv.circle(frame, (int(future_x), int(future_y)), 5, (255, 0, 0), -1)
                     if i==10:
                         target_frame = frame_count + 10
                         predictions[track_id].append((target_frame, future_x, future_y))
-
+                ade_predictions[track_id].append(forecast)
                 kalman_history[track_id].append((state_post[0][0], state_post[1][0]))
 
                 # print(f"Predicted state {detection[0]}, ID {track_id}: ", state_pre)
@@ -74,10 +81,34 @@ while cap.isOpened():
                         dx = predicted_x - x_center
                         dy = predicted_y - y_center
                         error = sqrt(dx**2 + dy**2)
-                        print(f"ID {track_id} | 10-frame error: {error} px")
-                        predictions[track_id].remove(prediction)
-
+                        if track_id not in fdes:
+                            fdes[track_id] = []
+                        fdes[track_id].append(error)
+                        # print(f"ID {track_id} | 10-frame error: {error} px")
+                        predictions[track_id].remove(prediction)                    
             # print(f"{detection[0]}| id: {track_id}| confidence: {detection[1]} | box: {detection[2]}")
+                
+                for forecast in ade_predictions[track_id]:
+                    for prediction in forecast["predictions"]:
+                        if prediction[0] == frame_count:
+                            if track_id not in ades:
+                                ades[track_id] = []
+                            predicted_x = prediction[1]
+                            predicted_y = prediction[2]
+                            dx = predicted_x - x_center
+                            dy = predicted_y - y_center
+                            error = sqrt(dx**2 + dy**2)
+                            forecast["errors"].append(error)
+                            if len(forecast["errors"]) == 10:
+                                ade = 0
+                                for error in forecast["errors"]:
+                                    ade += error
+                                ade /=10
+                                ades[track_id].append(ade)
+
+                            print(f"ID {track_id} | error: {error} px")
+
+
         for track_id, positions in history.items():
             for i in range(0, len(positions)):
                 cv.circle(frame, (int(positions[i][0]), int(positions[i][1])), 5, (0, 255, 0), -1)
@@ -93,7 +124,36 @@ while cap.isOpened():
 
     else:
         break
+total_error = 0
+total_predictions = 0  
+for track_id, errors in fdes.items():
+            sum_fde = 0
+            for i in range(0, len(errors)):
+                sum_fde += errors[i]
+            mean_fde = sum_fde / len(errors)
+            total_error += sum_fde
+            total_predictions += len(errors)
+            # print(f"Mean FDE for ID {track_id} = {mean_fde}")
 
+total_fde = total_error / total_predictions
+
+ade_total_error = 0
+ade_total_predictions = 0
+for track_id, errors in ades.items():
+    sum_ade = 0
+    for i in range(len(errors)):
+        sum_ade += errors[i]
+    if len(errors) > 0:
+        mean_ade = sum_ade / len(errors)
+        ade_total_error += sum_ade
+        ade_total_predictions += len(errors)
+        print(f"Mean ADE for ID {track_id} = {mean_ade}")
+
+total_ade = ade_total_error / ade_total_predictions
+print(f"Overall Mean ADE@10: {total_ade} px")
+print(f"Number of evaluated forecasts: {ade_total_predictions}")
+# print(f"Overall Mean FDE@10: {total_fde} px")
+# print(f"Number of evaluated predictions: {total_predictions}")
 # print("History of tracked objects:")
 # for track_id, positions in history.items():
 #     print(f"Track ID: {track_id}, Positions: {positions}")
